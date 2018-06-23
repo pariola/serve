e9945c8af90bdd6a257a923ad97d0c27bebedf04
Blessing Pariola <blessingpariola@zoho.com>
2018-06-23 16:13:39 +0100

    added yarn script

--- install.sh
+++ install.sh
@@ -8,5 +8,10 @@ curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs
 
+# Install Yarn
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
 # Done
 echo "Done"
